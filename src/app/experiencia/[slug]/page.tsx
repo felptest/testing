a27504f08de3a119eecb3fd4a6b1e9d1e580be99
vitorhfv <vitorhfv2@gmail.com>
2023-03-04 10:16:36 -0400

    update - add experiencia page and doc-items

---
+++ src/app/experiencia/[slug]/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+/* import styles from './page.module.css'
+ */
+
+
+export default function Experiment() {
+
+  return (
+    <>
+      <header /* className={styles.header} */>
+          <div>header</div>
+      </header>
+
+      <main /* className={styles.main} */>  
+        <div>main</div>
+      </main>
+
+      <footer /* className={styles.footer} */>
+        <div>footer</div>
+      </footer>
+    </>
+  
+  )
+}
