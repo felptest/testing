d33198138d6eed53fdf902d1a6173c4140c21ae4
vitorhfv <vitorhfv2@gmail.com>
2023-03-04 16:59:27 -0400

    Update experencia page

--- src/app/experiencia/[slug]/page.tsx
+++ src/app/experiencia/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-/* import styles from './page.module.css'
- */
+ import styles from './page.module.css'
+ 
 
 
 export default function Experiment() {
@@ -11,8 +11,22 @@ export default function Experiment() {
           <div>header</div>
       </header>
 
-      <main /* className={styles.main} */>  
-        <div>main</div>
+      <main  className={styles.main} >  
+        <div className={styles.mainContent}>
+          <div className={styles.mainHeader}>
+            <div className={styles.mainHeaderProfileImageContainer}>
+              <img src="https://4maos.com.br/wp-content/uploads/2022/07/f2582064395492d2a0f0d0f2baded609.jpg" alt="" />
+            </div>
+              <span className={styles.mainHeaderProfileNameContainer}><p>Felippe Matheus</p></span>
+        
+              <p>04 de março de 2023</p>
+          </div>
+
+          <div className={styles.ContentPost}>
+            <h2>Meu grande titulo</h2>
+          </div>
+
+        </div>
       </main>
 
       <footer /* className={styles.footer} */>
