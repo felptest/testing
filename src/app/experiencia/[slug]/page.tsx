7a4b7b1454b08e2f864fdfe2a466be9b76e59343
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2023-03-04 20:22:59 -0400

    update experiencia page

--- src/app/experiencia/[slug]/page.tsx
+++ src/app/experiencia/[slug]/page.tsx
@@ -13,21 +13,61 @@ export default function Experiment() {
 
       <main  className={styles.main} >  
         <div className={styles.mainContent}>
+
+        <div className={styles.conteudoTuto}>
+            <div className={styles.parallax}></div>
+        </div>
+         
           <div className={styles.mainHeader}>
-            <div className={styles.mainHeaderProfileImageContainer}>
-              <img src="https://4maos.com.br/wp-content/uploads/2022/07/f2582064395492d2a0f0d0f2baded609.jpg" alt="" />
-            </div>
+            <div className={styles.mainHeaderContentLeft}>
+              <div className={styles.mainHeaderProfileImageContainer}>
+                <img src="https://4maos.com.br/wp-content/uploads/2022/07/f2582064395492d2a0f0d0f2baded609.jpg" alt="" />
+              </div>
               <span className={styles.mainHeaderProfileNameContainer}><p>Felippe Matheus</p></span>
         
-              <p>04 de março de 2023</p>
+            </div>
+            
+            <div className={styles.mainHeaderContentRight}>
+              <p>Postado em: </p>
+              <span><p>04 de março de 2023</p></span>
+              
+            </div>
           </div>
 
           <div className={styles.ContentPost}>
+
             <h2>Meu grande titulo</h2>
+            <p>essa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descrição</p>
+          
+            <h3>Materiais utilizados</h3>
+            <ul>
+              <li>material 1</li>
+              <li>material 2</li>
+              <li>material 3</li>
+              <li>material 4</li>
+              <li>material 5</li>
+
+            </ul>
+
+            <h3>Instruções</h3>
+            <ul>
+              <li>Nós devemos fazerNós devemos fazerNós devemos fazerNós devemos fazer</li>
+              <li>Nós devemos fazerNós devemos fazerNós devemos fazerNós devemos fazerevemos fazerNós devemos fazerNós devemos fazerNós devemos fazerevemos fazerNós devemos fazerNós devemos fazerNós devemos fazerevemos fazerNós devemos fazerNós devemos fazerNós devemos fazer</li>
+              <li>Nós devemos fazerNós devemos fazerNós devemos fazerNós devemos fazer</li>
+              <li>Nós devevemos fazerNós devemos fazerNós devemos fazerNós devemos fazerevemos fazerNós devemos fazerNós devemos fazerNós devemos fazerevemos fazerNós devemos fazerNós devemos fazerNós devemos fazeremos fazerNós devemos fazerNós devemos fazerNós devemos fazer</li>
+            </ul>
+
+            <h3>Explicação ciêntifica da experiência</h3>
+            <p>A explicação é issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjss</p>
+          
+          
           </div>
 
         </div>
       </main>
+
+
+
 
       <footer /* className={styles.footer} */>
         <div>footer</div>
