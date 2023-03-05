870f72ab96e26a6c1f067beab36394aa34b3bcd0
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2023-03-05 14:54:54 -0400

    update experiencia page

--- src/app/experiencia/[slug]/page.tsx
+++ src/app/experiencia/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default function Experiment() {
           <div className={styles.mainHeader}>
             <div className={styles.mainHeaderContentLeft}>
               <div className={styles.mainHeaderProfileImageContainer}>
-                <img src="https://4maos.com.br/wp-content/uploads/2022/07/f2582064395492d2a0f0d0f2baded609.jpg" alt="" />
+                <img src="https://hips.hearstapps.com/hmg-prod/images/street-portrait-of-a-young-man-using-mobile-phone-royalty-free-image-1018047498-1564431457.jpg?crop=0.668xw:1.00xh;0.226xw,0&resize=1200:*" alt="" />
               </div>
               <span className={styles.mainHeaderProfileNameContainer}><p>Felippe Matheus</p></span>
               <p>Postado em: </p>
@@ -40,6 +40,9 @@ export default function Experiment() {
             <h2>Meu grande titulo</h2>
             <p>essa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descriçãoessa e a descrição</p>
           
+            <h3>Revisão de literatura</h3>
+            <p>Essa experiência é do tema isso e isso e fala queEssa experiência é do tema isso e isso e fala queEssa experiência é do tema isso e isso e fala queEssa experiência é do tema isso e isso e fala que </p>
+
             <h3>Materiais utilizados</h3>
             <ul>
               <li>material 1</li>
@@ -61,9 +64,20 @@ export default function Experiment() {
             <h3>O que deve acontecer?</h3>
             <p>Conforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer deConforme for fazendo deve ocorrer de</p>
 
-            <h3>Explicação ciêntifica da experiência</h3>
+            <h3>Explicação ciêntifica</h3>
             <p>A explicação é issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjssé issso e issos jfjfhfgdjss</p>
-          
+
+            <h3>Conclusões</h3>
+            <p>Concluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos queConcluimos que </p>
+
+            <h3>Referências bibliográficas</h3>
+            <ul>
+              <li>link de site numero 1</li>
+              <li>link de site numero 2</li>
+              <li>link de site numero 3</li>
+              <li>link de site numero 4</li>
+              <li>link de site numero 5</li>
+            </ul>
           
           </div>
 
