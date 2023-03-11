4a811af9a32e0ee37f37cc295ed0452045995bb6
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2023-03-11 15:50:35 -0400

    add in home page with search filter input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,47 @@ import Link from 'next/link'
 
 import experimentData from "../app/api/data/experimentos.json"
 import { useState } from 'react'
-import { SearchForm } from '@/components/searchForm';
+
+import {useRouter, useSearchParams } from "next/navigation"
 
 
+import bnccData from "../app/api/data/bncc.json"
+import topicGeneralData from "../app/api/data/experimentGeneralData.json"
+import topicSpecificData from "../app/api/data/materias.json"
+
+//PRECISO ver o visual de experiment data, e talvez adicionar um search de escrever!
 export default function Home() {
 
+  const [experimentBnccData] = useState(bnccData)
+  const [experimentGeneralData] = useState(topicGeneralData)
+  const [experimentSpecificData] = useState(topicSpecificData)
 
-  const [experimentInfo] = useState(experimentData)
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [selectedValues, setSelectedValues] = useState({
+    topicGeneral: searchParams.get('topicGeneral') || '',
+    topicSpecific: searchParams.get('topicSpecific') || '',
+    topicBncc: searchParams.get('topicBncc') || '',
+  });
+  
+  const filteredData = experimentData.filter(
+    (experiment) =>
+      (!selectedValues.topicGeneral || experiment.topicGeneral === selectedValues.topicGeneral) &&
+      (!selectedValues.topicSpecific || experiment.topicSpecific === selectedValues.topicSpecific) &&
+      (!selectedValues.topicBncc || experiment.topicBncc === selectedValues.topicBncc)
+  );
+
+  const handleResetFilter = () => {
+    setSelectedValues({
+      topicGeneral: '',
+      topicSpecific: '',
+      topicBncc: '',
+    });
+    setNumToShow(6);
+    router.push('/');
+  }
+
 
   const [numToShow, setNumToShow] = useState(6);
 
@@ -24,8 +58,8 @@ export default function Home() {
   }
 
   const sortedExperiments = recentClicked
-  ? experimentInfo.slice().reverse()
-  : experimentInfo; 
+  ? filteredData.slice().reverse()
+  : filteredData; 
 
   const handleRecentClick = () => {
     if (!recentClicked) {
@@ -55,27 +89,96 @@ export default function Home() {
         <h1>Encontre os <span>melhores experimentos</span> de ciências</h1>
       </div>
 
-      <SearchForm />
-
+      <div>
+        <h2>nessa div vai ficcar uma introdução</h2>
+      </div>
 
     </div>
       </div>
 
       <main className={styles.main}>
-      <div className={styles.content}>
+
+      <div>
+        <h1>Experiment data</h1>
+
+        <select
+          value={selectedValues.topicGeneral}
+          onChange={(e) => {
+            const newTopicGeneral = e.target.value;
+            setSelectedValues({
+              ...selectedValues,
+              topicGeneral: newTopicGeneral,
+            });
+            router.push(`/?topicGeneral=${newTopicGeneral}&topicSpecific=${selectedValues.topicSpecific}&topicBncc=${selectedValues.topicBncc}`);
+          }}
+        >
+          <option value="">Select a general topic</option>
+          {experimentGeneralData.map((topic) => (
+            <option key={topic.id} value={topic.title}>
+              {topic.title}
+            </option>
+          ))}
+        </select>
+
+        <select
+          value={selectedValues.topicSpecific}
+          onChange={(e) => {
+            const newTopicSpecific = e.target.value;
+            setSelectedValues({
+              ...selectedValues,
+              topicSpecific: newTopicSpecific,
+            });
+            router.push(`/?topicGeneral=${selectedValues.topicGeneral}&topicSpecific=${newTopicSpecific}&topicBncc=${selectedValues.topicBncc}`);
+          }}
+        >
+          <option value="">Select a specific topic</option>
+          {experimentSpecificData.map((topic) => (
+            <option key={topic.id} value={topic.title}>
+              {topic.title}
+            </option>
+          ))}
+        </select>
+
+         <select
+          value={selectedValues.topicBncc}
+          onChange={(e) => {
+            const newTopicBncc = e.target.value;
+            setSelectedValues({
+              ...selectedValues,
+              topicBncc: newTopicBncc,
+            });
+            router.push(`/?topicGeneral=${selectedValues.topicGeneral}&topicSpecific=${selectedValues.topicSpecific}&topicBncc=${newTopicBncc}`);
+          }}
+        >
+          <option value="">Select a BNCC topic</option>
+          {experimentBnccData.map((topic) => (
+            <option key={topic.id} value={topic.title}>
+              {topic.title}
+            </option>
+          ))}
+        </select>
+ 
+        <button onClick={handleResetFilter}>Reset Filter</button>       
+
+      </div>
+
+
+
+       <div className={styles.content}>
         <section className={styles.top}>
           <h2>Experimentos recomendados</h2>
           <div className={styles.view}>
 
           <button className={recentClicked ? styles["btn-primary-active"] : styles["btn-primary"]} onClick={handleRecentClick}>
-            Últimos
+            Mais recentes
           </button>
           <button className={latestClicked ? styles["btn-secondary-active"] : styles["btn-secondary"]} onClick={handleLatestClick}>
-            Recentes
+            Mais antigos
           </button>
 
           </div>
         </section>
+
         <section className={styles.cards}>
           {sortedExperiments === undefined && (
             <div>
@@ -110,7 +213,7 @@ export default function Home() {
             <button onClick={handleClick}>Carregar mais</button>
           )}
         </section>
-      </div>
+      </div> 
     </main>
 
     </>
