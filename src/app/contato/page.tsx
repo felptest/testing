7b1bff19e12741f1c21eaba89570e9bf1fda213c
vitorhfv <vitorhfv2@gmail.com>
2023-04-06 21:16:45 -0400

    Update pages app

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import styles from './page.module.css'
 
 
 export default function Contact() {  
@@ -7,10 +8,23 @@ export default function Contact() {
 
 
   return (
-    <>
-    <div>pagina de contatos</div>
-    </>
-
-  
-  )
+    <div className={styles.container}>
+      <h2 className={styles.title}>Entre em contato conosco</h2>
+      <form className={styles.form}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name" className={styles.label}>Nome:</label>
+          <input id="name" type="text" className={styles.input} required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email" className={styles.label}>E-mail:</label>
+          <input id="email" type="email" className={styles.input} required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="message" className={styles.label}>Mensagem:</label>
+          <textarea id="message" className={styles.textarea} required></textarea>
+        </div>
+        <button type="submit" className={styles.button}>Enviar</button>
+      </form>
+    </div>
+  );
 }
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import styles from './page.module.css'
+import image from '../../assets/01.jpeg'
+import Link from 'next/link';
 
 export default function About() {  
  
@@ -7,10 +9,23 @@ export default function About() {
 
 
   return (
-    <>
-    <div>pagina sobre</div>
-    </>
-
-  
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Sobre nós</h1>
+      <div className={styles.content}>
+        <div className={styles.description}>
+          <h2>Quem somos</h2>
+          <p>Somos uma equipe apaixonada por tecnologia e inovação. Desde a nossa fundação, em 2010, temos como objetivo criar soluções criativas e eficientes para ajudar nossos clientes a alcançarem seus objetivos.</p>
+          <p>Nossa equipe é composta por profissionais altamente qualificados e experientes, que estão sempre em busca de novos desafios e aprendizados. Estamos sempre atualizados sobre as últimas tendências e tecnologias do mercado para oferecer as melhores soluções aos nossos clientes.</p>
+        </div>
+        <div className={styles.image}>
+          <img src="https://www.poderdaescuta.com/wp-content/uploads/2018/07/210765-produtividade-de-sua-equipe-6-maneiras-de-melhorar.jpg" />
+        </div>
+      </div>
+      <div className={styles.cta}>
+        <h2>Entre em contato</h2>
+        <p>Estamos prontos para ajudar você e sua empresa a alcançarem seus objetivos. Entre em contato conosco para saber mais sobre nossos serviços e soluções.</p>
+        <button className={styles.button}><Link href="/contato">Entre em contato</Link></button>
+      </div>
+    </div>
+  );
 }
